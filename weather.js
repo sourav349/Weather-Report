7d0db3485454c6acb3997a840c4826173228dd64
SOURAV KUMAR <146092564+sourav349@users.noreply.github.com>
2024-09-08 21:20:44 +0530

    Weather

---
+++ weather.js
@@ -0,0 +1,56 @@
+const apiKey = "de81d6d50861459ad0392f8e0b6defd0";
+        const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+     
+        const searchBox = document.querySelector(".search input");
+        const searchBtn = document.querySelector(".search button");
+        const weatherIcon = document.querySelector(".weather-icon");
+    
+        async function checkWeather(city){
+        const response =await fetch(apiUrl+ city+`&appid=${apiKey}`);
+       
+        if(response.status == 404 ){
+            document.querySelector(".error").style.display ="block";
+            document.querySelector(".weather").style.display ="none";
+        }
+        else{
+        var data = await response.json();
+
+        // console.log(data);
+        
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity+"%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+       
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "file:///C:/Users/shivani_agarwal/Desktop/vs%20code/weather%20project/image/clouds.png";
+        }
+        else if(data.weather[0].main == "Clear")
+            {
+                weatherIcon.src = "file:///C:/Users/shivani_agarwal/Desktop/vs%20code/weather%20project/image/clear.png";
+            }
+            else if(data.weather[0].main == "Rain")
+                {
+                    weatherIcon.src = "file:///C:/Users/shivani_agarwal/Desktop/vs%20code/weather%20project/image/rain.png";
+                }
+                else if(data.weather[0].main == "Drizzle")
+                    {
+                        weatherIcon.src = "file:///C:/Users/shivani_agarwal/Desktop/vs%20code/weather%20project/image/drizzle.png";
+                    }
+                    else if(data.weather[0].main == "Mist")
+                        {
+                            weatherIcon.src = "file:///C:/Users/shivani_agarwal/Desktop/vs%20code/weather%20project/image/mist.png";
+                        }
+                        else if(data.weather[0].main == "Snow")
+                            {
+                                weatherIcon.src = "file:///C:/Users/shivani_agarwal/Desktop/vs%20code/weather%20project/image/snow.png";
+                            }
+                 document.querySelector(".weather").style.display = "block" ;
+                 document.querySelector(".error").style.display ="none";
+                }           
+    }
+
+    searchBtn.addEventListener("click", ()=>{
+        checkWeather(searchBox.value);
+    })
+    
